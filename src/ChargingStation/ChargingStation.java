0063ed637be0caeb94df4e799dcbbf596aa0499f
Tsvety Sotonov <tsotonov2604@yahoo.com>
2021-10-10 14:53:37 -0500

    first commit

---
+++ src/ChargingStation/ChargingStation.java
@@ -0,0 +1,40 @@
+package ChargingStation;
+
+//import floor plan grid
+//import battery info  
+
+public class ChargingStation implements IChargingStation {
+
+    //private List<Grid Type> CHARGING_STATION_LOCATION; 
+    //public List<Grid Type> currentLocation;
+    //private MAX_BATTERY_CAPACITY = 100;
+    //public int currentCharge = battery info from import; 
+   
+
+   public void setChargingStationLocation(){
+       //set initial coordinates of charging station based on floor plan grid
+       //CHARGING_STATION_LOCATION  = initial location; 
+   }
+
+   public void getChargingStationLocation(){
+       //return setChargingStationLocation();      
+   }
+
+   public void navigateToChargingStation(){
+        //currentLocation = getChargingStationLocation()       
+   }
+
+   public void setCurrentLocation(){
+       //get the current location of the clean sweep from grid
+       //current location= current x,y coordinates from grid 
+   }
+  
+   public void getCurrentLocation(){
+       //return get the current location of the clean sweep from grid
+   }
+
+   public void charge(){
+       //currentCharge= MAX_BATTERY_CAPACITY
+       System.out.println("Charging!");
+   }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import ChargingStation.ChargingStation;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        ChargingStation cs = new ChargingStation(); 
+        cs.charge();
+    }
+}
