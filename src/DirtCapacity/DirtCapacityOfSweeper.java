3b51e52719ef48bb4a3cc157c2d83d6849f6b546
SALANGARDEPAUL <salangar@depaul.edu>
2021-10-10 19:51:02 -0500

    Initiate DirtCapacityOfSweeper class.

---
+++ src/DirtCapacity/DirtCapacityOfSweeper.java
@@ -0,0 +1,17 @@
+package DirtCapacity;
+
+public class DirtCapacityOfSweeper{
+
+
+    private static final int MaxDirtCapacity = 100;
+
+// ToDo
+    public void checkBagSize(){
+
+        }
+    }
+
+
+
+
+
