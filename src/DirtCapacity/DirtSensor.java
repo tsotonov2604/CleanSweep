f0005997fb274c70c78d5fca533682e2eda35dbc
SALANGARDEPAUL <salangar@depaul.edu>
2021-10-16 15:35:31 -0500

    Refactoring DirtSensor class to satisfy GUI.

--- src/DirtCapacity/DirtSensor.java
+++ src/DirtCapacity/DirtSensor.java
@@ -1,17 +1,28 @@
 package DirtCapacity;
+import javax.swing.*;
 
-public class DirtSensor{
+
+public class DirtSensor {
 
     DirtCapacityOfSweeper sensor = new DirtCapacityOfSweeper();
-    private static final int MaxCapacity = 100;
-    public boolean Capacity;
+    public int Capacity;
 
     public DirtSensor(){}
 
-    public void aFulledBag() {
-         Capacity = (sensor.getCurrentBagSize() == MaxCapacity);
-        if (Capacity) {
-            System.out.println("The bag is full!");
+    public void FullBag() {
+        Capacity = sensor.getCurrentBagSize();
+        if (Capacity == 100) {
+            JOptionPane.showMessageDialog(null, "The bag is full!",
+                    "WARNING", JOptionPane.WARNING_MESSAGE);
         }
     }
+        public void EmptyBag(){
+            Capacity = sensor.getCurrentBagSize();
+            if (Capacity == 0) {
+                JOptionPane.showMessageDialog(null, "The bag is Empty!",
+                        "INFO", JOptionPane.INFORMATION_MESSAGE);
+            }
+     }
+
+
 }
