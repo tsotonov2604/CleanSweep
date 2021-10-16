e674b723f9494c270d53d76d7c2503f8c4ae7e42
SALANGARDEPAUL <salangar@depaul.edu>
2021-10-16 14:26:43 -0500

    Modifying the OnOffButton class to be related to the Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 import ChargingStation.ChargingStation;
+import Power.OnOffButton;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        ChargingStation cs = new ChargingStation(); 
+        ChargingStation cs = new ChargingStation();
         cs.charge();
+
+        //OnOffButton
+        OnOffButton OnOff = new OnOffButton();
+        OnOff.setTitle("Turning a device On/Off");
+        OnOff.setLayout(new FlowLayout());
+        OnOff.setJToggleButton();
+        OnOff.setAction();
+        OnOff.setSize(300, 200);
+        OnOff.setVisible(true);
+        OnOff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
--- src/Power/OnOffButton.java
+++ src/Power/OnOffButton.java
@@ -1,30 +1,19 @@
 package Power;
 
-import java.awt.FlowLayout;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import javax.swing.JFrame;
 import javax.swing.JToggleButton;
 
 public class OnOffButton extends JFrame implements ItemListener {
-    public static void main(String[] args) {
-        new OnOffButton();
-    }
-    private JToggleButton button;
-    OnOffButton() {
-        setTitle("Allow a user to turn the device On/Off");
-        setLayout(new FlowLayout());
-        setJToggleButton();
-        setAction();
-        setSize(300, 200);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    private void setJToggleButton() {
+
+    public JToggleButton button;
+    public OnOffButton() {}
+    public void setJToggleButton() {
         button = new JToggleButton("ON");
         add(button);
     }
-    private void setAction() {
+    public void setAction() {
         button.addItemListener(this);
     }
     public void itemStateChanged(ItemEvent eve) {
