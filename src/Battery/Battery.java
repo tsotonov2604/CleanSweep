a81193b6ffb5411cb10d64d8be37106f9a153074
Fuddin21 <77081506+Fuddin21@users.noreply.github.com>
2021-10-16 20:10:30 -0500

    Update Battery.java

--- src/Battery/Battery.java
+++ src/Battery/Battery.java
@@ -5,15 +5,12 @@ public class Battery {
     private double CurrentBatteryPercent;
     private double LowBatteryPercent = 20;
 
-
-    public double GetBatteryPercent;{
-    public double GetBatteryPercent(){
+    public double GetBatteryPercent() {
 
         return CurrentBatteryPercent;
 
     }
 
-    public double LowBattery; {
     public void LowBattery() {
 
         if (CurrentBatteryPercent <= LowBatteryPercent) {
