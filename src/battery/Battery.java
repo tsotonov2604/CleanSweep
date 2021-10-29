c30073c108e52c70de87b062bea892f9175a7682
SALANGARDEPAUL <salangar@depaul.edu>
2021-10-28 22:21:22 -0500

    Informing a user about the battery's state.

--- src/battery/Battery.java
+++ src/battery/Battery.java
@@ -1,9 +1,11 @@
 package battery;
+import javax.swing.*;
 
 public class Battery {
 
     public double CurrentBatteryPercent;
     private double LowBatteryPercent = 20;
+    private double FullBatteryPercent=250;
 
     public double getCurrentBatteryPercent() {
         return this.CurrentBatteryPercent;
@@ -22,5 +24,20 @@ public class Battery {
         return true;
     }
 
+    public void LowBatteryWar() {
+
+        if (CurrentBatteryPercent <= LowBatteryPercent) {
+            JOptionPane.showMessageDialog(null, "Low Battery",
+                    "WARNING", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    public void fullBatteryInfo() {
+
+        if (CurrentBatteryPercent == FullBatteryPercent) {
+            JOptionPane.showMessageDialog(null, "Full Battery",
+                    "INFO", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
 
 }
