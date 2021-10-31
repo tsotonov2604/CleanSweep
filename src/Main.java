ee711b3dce562b5d8a8ee427713c5f104e87edba
salemij93 <salemi-j@hotmail.com>
2021-10-31 17:57:32 -0500

    re-added dialog to main class

--- src/Main.java
+++ src/Main.java
@@ -73,6 +73,24 @@ public class Main {
         war.FullBag();
         war.EmptyBag();
 
+        //schedule
+        JLabel label = new JLabel("Selected Date:");
+        final JTextField text = new JTextField(20);
+        JButton b = new JButton("popup");
+        JPanel p = new JPanel();
+        p.add(label);
+        p.add(text);
+        p.add(b);
+        final JFrame f = new JFrame();
+        f.getContentPane().add(p);
+        f.pack();
+        f.setVisible(true);
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                text.setText(new Schedule(f).setPickedDate());
+            }
+        });
+
 
 
 
