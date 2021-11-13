1709333ba0dfc76b0dc7fb2923192f3943cab5cd
Tsvety Sotonov <tsotonov2604@yahoo.com>
2021-11-13 12:41:37 -0600

    charging station unit tests

--- src/test/java/BatteryTest.java
+++ src/test/java/BatteryTest.java
@@ -1,8 +1,7 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import battery.Battery;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BatteryTest {
     Battery battery;
